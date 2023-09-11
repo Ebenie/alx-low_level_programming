cef5a65cddf4c9a949e12c540605445b8d2c3650
Ebenie <abrahamavenue163@.com>
2023-09-11 11:08:31 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,21 +5,22 @@
 
 
 /**
-   * main - Entry point 
+   * main - Entry point
    *Return: Always (Success)
    *
    */
 
 int main(void)
 {
-  
+
 int d;
 
 
 for (d = '0'; d <= '9'; d++)
 {
 putchar(d);
-if (d != '9'){
+if (d != '9')
+{
 putchar(',');
 putchar(' ');
 }
