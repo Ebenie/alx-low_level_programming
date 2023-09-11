62ce27760fb1d69a860be6bfa900c391db803741
Ebenie <abrahamavenue163@.com>
2023-09-11 15:09:51 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+
+
 /**
-  * main - Prints "with proper grammar, but the outcome is a piece of art"
-  * Return : Always (Success)
- */
+   * main - Prints "with proper grammar, but the outcome is a piece of art"
+   *Return: Always (Success)
+   *
+   */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
