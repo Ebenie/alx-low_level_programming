b484290a2e4437511a3f2b2f136b5eb780d45004
Ebenie <abrahamavenue163@.com>
2023-09-11 12:17:26 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+
+/**
+   * main - Print if number is positive, zero or negative
+   *Return: Always (Success)
+   *
+   */
+
+int main(void)
+{
+
+
+int i;
+
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
+
+return (0);
+}
