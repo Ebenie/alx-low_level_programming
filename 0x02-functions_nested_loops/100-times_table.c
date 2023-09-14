167b915b1e8be031d3ccb19a73db0f9cd874651c
Ebenie <abrahamavenue163@.com>
2023-09-14 14:20:15 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main: - check the code
+* Return: Always 0.
+*
+*/
+
+int main(void)
+{
+print_times_tabel(3);
+_putchar('\n');
+print_times_tabel(5);
+_putchar('\n');
+print_times_tabel(98);
+_putchar('\n');
+print_times_tabel(12);
+return (0);
+
+}
