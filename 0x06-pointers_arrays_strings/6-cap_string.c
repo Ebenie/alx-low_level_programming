7063d67a5bfd5c4efe3dd545ad9e19dba30acf6e
Ebenie <abrahamavenue163@.com>
2023-09-21 15:17:36 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *str)
 
         while (str[index])
         {
-                while (!(str[index] >= 'A' && str[index] <= 'Z'))
+                while (!(str[index] >= 'a' && str[index] <= 'z'))
                         index++;
 
 
