c2f82acdcb37b7fbf1e4ed94443aeaf81ba2b731
Ebenie <abrahamavenue163@.com>
2023-12-15 12:28:15 -0800

    This is second

---
+++ 0x18-dynamic_libraries/abs.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value
+ * Return: 0 always
+ */
+int _abs(int n)
+{
+
+return 0;
+}
---
+++ 0x18-dynamic_libraries/atoi.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include "main.h"
+/**
+ * _atoi - Converts a string to an integer
+ * Return: 0 always
+ */
+int _atoi(char *s)
+{
+
+return 0;
+}
