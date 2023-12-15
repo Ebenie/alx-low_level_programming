bfd48421af254d809548d111df22fff94184d3ca
Ebenie <abrahamavenue163@.com>
2023-12-15 12:09:29 -0800

    Again

---
+++ 0x18-dynamic_libraries/libdynamic.c
@@ -0,0 +1,214 @@
+#include "main.h"
+#include <stddef.h>
+
+
+/**
+ * _putchar - Writes a character to the standard output
+ * Return: 0 always
+ */
+int _putchar(char c)
+{
+
+return 0;
+}
+
+/**
+ * _islower - Checks if a character is a lowercase
+ * Return: 0 always
+ */
+int _islower(int c)
+{
+
+return 0;
+}
+
+
+/**
+ * _isalpha - Checks if a character is an alphabet
+ * Return: 0 always
+ */
+int _isalpha(int c)
+{
+
+return 0;
+}
+
+/**
+ * _abs - Computes the absolute value
+ * Return: 0 always
+ */
+int _abs(int n)
+{
+
+return 0;
+}
+
+
+
+/**
+ * _isupper - Checks if a character is an uppercase
+ * Return: 0 always
+ */
+int _isupper(int c)
+{
+
+return 0;
+}
+
+/**
+ * _isdigit - Checks if a character is a digit
+ * Return: 0 always
+ */
+int _isdigit(int c)
+{
+
+return 0;
+}
+
+/**
+ * _strlen - Calculates the length of a string
+ * Return: 0 always
+ */
+int _strlen(char *s)
+{
+
+return 0;
+}
+
+/**
+ * _puts - Writes a string to the standard output
+ * Return: 0 always
+ */
+void _puts(char *s)
+{
+
+}
+
+/**
+ * _strcpy - Copies a string
+ * Return: 0 always
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+return NULL;
+}
+
+
+/**
+ * _atoi - Converts a string to an integer
+ * Return: 0 always
+ */
+int _atoi(char *s)
+{
+
+return 0;
+}
+
+
+/**
+ * _strcat - Concatenates two strings
+ * Return: NULL always
+ */
+char *_strcat(char *dest, char *src)
+{
+
+return NULL;
+}
+
+/**
+ * _strncat - Concatenates n characters
+ * Return: NULL always
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+return NULL;
+}
+
+
+
+/**
+ * _strncpy - Copies n characters
+ * Return: NULL always
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+return NULL;
+}
+
+
+/**
+ * _strcmp - Compares two strings
+ * Return: 0 always
+ */
+int _strcmp(char *s1, char *s2)
+{
+
+return 0;
+}
+
+/**
+ * _memset - Fills the first n bytes of memory
+ * Return: NULL always
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+
+return NULL;
+}
+
+
+/**
+ * _memcpy - Copies n bytes from source memory
+ * Return: NULL always
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+return NULL;
+}
+
+
+/**
+ * _strchr - Locates the first occurrence
+ * Return: NULL always
+ */
+
+char *_strchr(char *s, char c)
+{
+
+return NULL;
+}
+
+/**
+ * _strspn - Calculates the length of the initial segment
+ * Return: 0 always
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+return 0;
+}
+
+/**
+ * _strpbrk - Locates the first occurrence
+ * Return: NULL always
+ */
+char *_strpbrk(char *s, char *accept)
+{
+
+return NULL;
+}
+
+/**
+ * _strstr - Locates the first occurrence of a substring
+ * Return: NULL always
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+return NULL;
+}
